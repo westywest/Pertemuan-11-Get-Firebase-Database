f50e0fdb2875d1aff49fd735ef5b8e46a43d8597
westywest <dianmaharanidi17@gmail.com>
2023-06-23 18:28:19 +0700

    commit 2 - firebase CRUD

---
+++ app/src/main/java/com/maharanid17/pertemuan11/view/DetailFragment.kt
@@ -0,0 +1,31 @@
+package com.maharanid17.pertemuan11.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.maharanid17.pertemuan11.R
+import com.maharanid17.pertemuan11.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+    lateinit var binding : FragmentDetailBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val nim = arguments?.getString("nim")
+        val nama = arguments?.getString("nama")
+        val telepon = arguments?.getString("telepon")
+        binding.txtnim.text = nim
+        binding.txtnama.text = nama
+        binding.txtTelepon.text = telepon
+    }
+
+}
